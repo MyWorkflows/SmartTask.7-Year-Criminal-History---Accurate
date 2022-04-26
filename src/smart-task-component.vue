6a0ba22b89edbdb70279ddeada7f879f8ec389d1
Enrico Willemse <enricowillemse.was@gmail.com>
2022-04-26 14:10:44 +0800

    Initial commit

---
+++ src/smart-task-component.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default /*#__PURE__*/ defineComponent({
+  name: "SmartTaskComponent", // vue component name
+  props: ['componentState', 'componentStatus'],
+  data() {
+    return { model: {} };
+  },
+  computed: {},
+  methods: {
+    async submitForm() {
+      this.$emit("execute-smart-task", this.model);
+    },
+  },
+  created() {
+    if (this.componentState) {
+      this.model = this.componentState;
+    }
+  },
+  setup() {},
+});
+</script>
+
+<template>
+  <form>
+    <div class="my-5">
+      <button
+        type="button"
+        @click="submitForm"
+        class="btn btn-sm btn-primary"
+      >
+       Mark as in Progress
+      </button>
+    </div>
+  </form>
+</template>
+
